c47af54329e0e4636400c2d40001a6b8affd3b4b
Slother <slothercode@gmail.com>
2024-03-14 17:51:54 +0100

    Added exercises

--- FunctionLambdaExpression.kt
+++ FunctionLambdaExpression.kt
@@ -3,7 +3,12 @@ fun main() {
     //exerciseTwo()
     //exerciseThree()
     //exerciseFour()
-    exerciseFive()
+    //exerciseFive()
+    //exerciseSix()
+    //exerciseSeven()
+    //exerciseEight()
+    //exerciseNine()
+    exerciseTen()
 }
 
 private fun exerciseOne(){
@@ -84,4 +89,77 @@ private fun exerciseFive(){
         println(filters)
     }
     greaterOneHundred(numbers)
+}
+
+private fun exerciseSix(){
+    //Define una función llamada sumar_elementos que tome una lista de números y utilice
+    //una expresión lambda junto con la función reduce para devolver la suma de todos los
+    //elementos.
+
+    val numbers = listOf(2,4,6,7,9,10,24,56,78,33,31,83,99)
+
+    fun sumElements(numbs: List<Int>){
+        val sum = numbs.reduce { accumulator, i ->
+            accumulator + i
+        }
+        println(sum)
+    }
+    sumElements(numbers)
+}
+
+private fun exerciseSeven(){
+    //Escribe una función llamada contar_vocales que tome una cadena de texto y utilice
+    //una expresión lambda junto con la función filter para contar el número de vocales
+    //en la cadena.
+
+    val vocals = listOf('a', 'e', 'i', 'o', 'u')
+
+    fun countVocals(text: String){
+        val count = text.filter { c ->
+            vocals.contains(c)
+        }
+        println(count)
+    }
+
+    countVocals("albaricoque")
+}
+
+private fun exerciseEight(){
+    //Desarrolla una función llamada eliminar_duplicados que tome una lista y utilice
+    //una expresión lambda junto con la función set para eliminar los elementos duplicados
+    //y devolver una lista sin duplicados.
+
+    val numbers = listOf(1,2,3,6,7,8,1,2,3,9,100,24,100,64,53453)
+
+    fun deleteDuplicate(numbs: List<Int>){
+        val delete = numbs.toSet()
+        println(delete)
+    }
+    deleteDuplicate(numbers)
+}
+
+private fun exerciseNine(){
+    //Crea una función llamada encontrar_menor que tome una lista de números y utilice una
+    //expresión lambda junto con la función min para encontrar el número menor en la lista.
+
+    val numbers = listOf(1,2,3,6,7,8,1,2,3,9,100,24,100,64,53453)
+
+    fun searchMinor(numbs: List<Int>){
+        val minor = numbs.min()
+        println(minor)
+    }
+    searchMinor(numbers)
+}
+
+private fun exerciseTen(){
+    //Define una función llamada calcular_promedio que tome una lista de números y
+    //utilice una expresión lambda junto con la función sum y len para calcular el
+    //promedio de los elementos.
+
+    val numbers = listOf(1,2,3,6,7,8,1,2,3,9,100,24,100,64,53453)
+    fun calculateAverage(numbs: List<Int>){
+        val average = numbs.sum() / numbs.size
+        println(average)
+    }
+    calculateAverage(numbers)
 }
